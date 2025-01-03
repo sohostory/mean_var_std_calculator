436b7b2fb62c3e270016fb62a8a5b6ebfbe72335
Hoyoung Lee <115523196+sohostory@users.noreply.github.com>
2025-01-03 14:42:03 -0600

    Add files via upload

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,34 +1,20 @@
 import numpy as np
 
 
-def calculate(list):
-    if len(list) != 9:
+def calculate(input_list):
+    if len(input_list) != 9:
         raise ValueError("List must contain nine numbers.")
 
-    calculations = {
-        'mean': [],
-        'variance': [],
-        'standard deviation': [],
-        'max': [],
-        'min': [],
-        'sum': []
-    }
-    np_array = np.array(list)
-    np_array = np_array.reshape((3, 3))
+    np_array = np.array(input_list).reshape((3, 3))
 
-    for i in range(2):
-        calculations['mean'].append(np_array.mean(axis=i).tolist())
-        calculations['variance'].append(np_array.var(axis=i).tolist())
-        calculations['standard deviation'].append(np_array.std(axis=i).tolist())
-        calculations['max'].append(np_array.max(axis=i).tolist())
-        calculations['min'].append(np_array.min(axis=i).tolist())
-        calculations['sum'].append(np_array.sum(axis=i).tolist())
+    calculations = {key: [] for key in ['mean', 'variance', 'standard deviation', 'max', 'min', 'sum']}
 
-    calculations['mean'].append(float(np_array.mean()))
-    calculations['variance'].append(float(np_array.var()))
-    calculations['standard deviation'].append(float(np_array.std()))
-    calculations['max'].append(int(np_array.max()))
-    calculations['min'].append(int(np_array.min()))
-    calculations['sum'].append(int(np_array.sum()))
+    for axis in [0, 1, None]:
+        calculations['mean'].append(np_array.mean(axis=axis).tolist())
+        calculations['variance'].append(np_array.var(axis=axis).tolist())
+        calculations['standard deviation'].append(np_array.std(axis=axis).tolist())
+        calculations['max'].append(np_array.max(axis=axis).tolist())
+        calculations['min'].append(np_array.min(axis=axis).tolist())
+        calculations['sum'].append(np_array.sum(axis=axis).tolist())
 
     return calculations
